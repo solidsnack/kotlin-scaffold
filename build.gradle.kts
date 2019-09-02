01a1c41d1b3152e1ae6e9e5128ba32fe4b410440
Jason Dusek <jason.dusek@gmail.com>
2019-09-02 16:43:22 -0700

    Buildable project

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+group = "example"
+version = "1.0-SNAPSHOT"
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+plugins {
+    kotlin("jvm") version "1.3.50"
+}
+
+repositories {
+    mavenCentral()
+}
+
---
+++ src/main/kotlin/example/isokt/Main.kt
@@ -0,0 +1,5 @@
+package example.isokt
+
+fun main() {
+    System.err.println("Hello, STDERR.")
+}
