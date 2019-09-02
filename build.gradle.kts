e1f35f7ff7ec2aa06244ab23094b189c3993fd25
Jason Dusek <jason.dusek@gmail.com>
2019-09-02 16:44:31 -0700

    Add & enable shadowJar

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,8 @@ dependencies {
 }
 
 plugins {
+    application
+    id("com.github.johnrengelman.shadow") version "5.1.0"
     kotlin("jvm") version "1.3.50"
 }
 
@@ -13,3 +15,6 @@ repositories {
     mavenCentral()
 }
 
+application {
+    mainClassName = "${group}.${name}.MainKt"
+}
